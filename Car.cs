d2dc387d7bd89b2a268313f3ba861588d1da82bc
macieowens <112588931+macieowens@users.noreply.github.com>
2022-12-05 13:20:10 -0600

    initial commit'

---
+++ Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classes
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Schema;
+
+namespace classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Car myCar = new Car();
+            myCar.Make = "Tesla";
+            myCar.Model = "Plaid";
+            myCar.Year = 2022;
+
+            var chevy = new Car("Chevy", "Impala", 2002);
+
+            var carList = new List<Car>() { myCar, chevy, new Car(), new Car() };
+
+            Console.WriteLine(carList.Capacity);
+        }
+    }
+}
